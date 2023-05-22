d1a5256600a1084f79383cd5ad89773592dcfdaf
jpramil <julien.pramil@insee.fr>
2023-05-22 09:51:13 +0000

    add seq4

---
+++ seq4.R
@@ -0,0 +1,53 @@
+naissances
+naissances <- dplyr::as_tibble(naissances)
+naissances
+
+# Mutate 
+
+naissances2 <- naissances %>% mutate(age2 = as.integer(agemere))
+naissances2 %>% select(agemere, age2)
+
+naissances2 <- naissances %>% 
+  mutate(agemere_0 = agemere) %>% 
+  mutate(agemere = as.integer(agemere))
+naissances2 %>% select(agemere_0, agemere)
+
+# ifelse
+
+naissances %>% 
+  mutate(sexe = ifelse(sexe == 1, yes = "garçon", no = "fille"))
+
+# Casewhen
+
+# Exercice ------------------------------------------------------------------
+
+naissances %>% 
+  mutate(
+    tagemere = 
+      case_when(
+        agemere < 20 ~ "moins de 20 ans" , 
+        agemere < 25  ~ "de 20 à moins de 25 ans" , 
+        agemere < 35  ~ "de 25 à moins de 35 ans" ,
+        T ~ "35 ans et plus"
+      )
+  ) -> toto
+
+toto %>% select(agemere, tagemere)
+toto %>% count(tagemere)
+
+
+is.na(c("Marie","Yves",NA,"Jeanne"))
+
+naissances %>%
+  mutate(sexe = ifelse(sexe == '1',"garçon","fille")) %>% 
+  filter(is.na(sexe))
+
+
+
+
+
+
+
+
+
+
